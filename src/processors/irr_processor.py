Fix rbar calculation

src/processors/irr_processor.py
@@ -76,19 +76,24 @@ class IRRProcessor:
 
         hash_map: Dict[str, Dict[str, List[str]]] = defaultdict(dict)
         for rater_data in rater1_data.rows:
-            cleaned_data = rater_data.data
+            cleaned_data = rater_data.data.strip()
             hash_map[cleaned_data][label_1] = self._process_labels(rater_data.labels)
+            hash_map[cleaned_data][label_2] = []
         for rater_data in rater2_data.rows:
-            cleaned_data = rater_data.data
-            if rater_data.data in hash_map:
-                hash_map[cleaned_data][label_2] = self._process_labels(
-                    rater_data.labels
-                )
-            else:
+            cleaned_data = rater_data.data.strip()
+            if cleaned_data not in hash_map:
+                hash_map[cleaned_data] = {}
                 hash_map[cleaned_data][label_1] = []
-                hash_map[cleaned_data][label_2] = self._process_labels(
-                    rater_data.labels
-                )
+            hash_map[cleaned_data][label_2] = self._process_labels(
+                rater_data.labels
+            )
+
+        for data, users_labels in hash_map.items():
+            rater_1_labels = sorted(users_labels.get(label_1, []))
+            rater_2_labels = sorted(users_labels.get(label_2, []))
+            # compare the labels to see if they are strictly equal
+            if rater_1_labels != rater_2_labels:
+                print(f'{data}\nRater 1: {", ".join(rater_1_labels)}\nRater 2: {", ".join(rater_2_labels)}', end="\n----------------------------\n")
 
         df = DataFrame(
             {}, index=np.arange(len(hash_map)), columns=self.available_labels
@@ -98,8 +103,18 @@ class IRRProcessor:
             loc=0, column=self.config.data_column_name, value=pd.Series(hash_map.keys())
         )
 
+        df["num_rater"] = 0
+
         for data, users_labels in hash_map.items():
             row_idx = df[df[self.config.data_column_name] == data].index[0]
+            if len(users_labels[label_1]) == 0 or len(users_labels[label_2]) == 0:
+                print(data, end='\n--------MISSING DATA--------\n')
+
+            if len(users_labels[label_1]) > 0:
+                df.at[row_idx, "num_rater"] += 1
+            if len(users_labels[label_2]) > 0:
+                df.at[row_idx, "num_rater"] += 1
+
             for label in users_labels[label_1]:
                 df.at[row_idx, label] += 1
             for label in users_labels[label_2]:
@@ -118,7 +133,8 @@ class IRRProcessor:
             for row_idx, row_data in df.iterrows():
                 data = row_data[file_config.data_column_name]
                 labels = row_data[file_config.labels_column_name]
-                rater_data_rows.append(RaterDataRow(labels=labels, data=data))
+                if not pd.isna(labels) and not pd.isna(data):
+                    rater_data_rows.append(RaterDataRow(labels=labels, data=data))
 
         return RaterData(rows=rater_data_rows)
 
@@ -127,12 +143,9 @@ class IRRProcessor:
 
         n = len(agreement_table)
 
-        rbar = 0
-        # TODO: The following code can be uncommented for finding rbar > 2 (although needs tweeks)
-        for i in range(len(agreement_table)):
-            rbar += agreement_table.loc[i, self.available_labels].sum()
-        rbar /= n
-        # TODO: Account for unlabeled data with rbar
+        r_bar = sum([agreement_table.at[i, "num_rater"] for i in range(len(agreement_table))]) / n
+        # drop num_rater after calculating r_bar
+        agreement_table.drop(columns=["num_rater"], inplace=True)
 
         p_primea = 0
         total_num_ratings = 0
@@ -146,10 +159,10 @@ class IRRProcessor:
                 # TODO: Expand rbar_ik to factor in w_kl for more than just "nominal" weight function
                 rbar_ik = agreement_table[label].loc[agreement_table.index[i]]
 
-                p_aik = (r_ik * (rbar_ik - 1)) / (rbar * (r_i - 1))
+                p_aik = (r_ik * (rbar_ik - 1)) / (r_bar * (r_i - 1))
                 p_primea += p_aik
         p_primea /= n
-        p_a = (p_primea * (1 - 1 / (n * rbar))) + (1 / (n * rbar))
+        p_a = (p_primea * (1 - 1 / (n * r_bar))) + (1 / (n * r_bar))
 
         # PI_ks: an array of PI_k, the percentage of ratings that fell into category k
         PI_ks = []