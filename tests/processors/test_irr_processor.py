c681fdb1047de5b4293dc54fef5b371d39acc342
ketphan02 <tuankietvn@outlook.com>
2024-09-13 15:22:52 -0700

    Simplify tests

--- tests/processors/test_irr_processor.py
+++ tests/processors/test_irr_processor.py
@@ -15,28 +15,10 @@ class TestIRRProcessor(TestCase):
 
         """
         prj_root = Path(__file__).parent.parent.parent
-
-        # The following filepath is where the REAL data is being stored
-        self.filepath = prj_root / "data/csv/irr_first_pass/"
-        self.folder_1 = prj_root / "data/csv/irr_first_pass/rater1/"
-        self.folder_2 = prj_root / "data/csv/irr_first_pass/rater2/"
-
         # Fake data
         self.fake_filepath = prj_root / "tests/processors/irr_test_data/"
         self.fake_1 = prj_root / "tests/processors/irr_test_data/rater1/"
         self.fake_2 = prj_root / "tests/processors/irr_test_data/rater2/"
-
-    def test_calculate_irr_real(self) -> None:
-        _rater1_data = IRRProcessor.extract_rater_data(self.folder_1)
-        _rater2_data = IRRProcessor.extract_rater_data(
-            self.folder_2, RawDataFileConfig(labels_column_name="Code")
-        )
-        all_labels = IRRProcessor.process_all_labels_file(self.filepath / "labels.txt")
-
-        irr_processor = IRRProcessor(all_labels)
-        result = irr_processor.calculate_irr(_rater1_data, _rater2_data)
-
-        print("Krippendorf's alpha:\t", result, end="\n\n")
 
     def test_calculate_irr_fake(self) -> None:
         _rater1_data = IRRProcessor.extract_rater_data(
